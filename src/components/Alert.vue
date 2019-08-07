a9f1f5b4b4c0262f0fe478d3f8d7e166122c8775
Unknown <talles_of_destiny@yahoo.com.sg>
2019-08-07 22:44:33 +0900

    Created .env and .env.example. Added Alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<v-alert
+			:value='alertObj.showAlert'
+			type='error'
+			prominent
+			border='top'
+			dismissible
+			class='che-alert-error'
+		>
+			<div v-if='alertObj.msg'>{{ alertObj.msg }}</div>
+			<ul v-if='alertObj.msgArr.length'>
+				<li v-for='item in alertObj.msgArr' :key='item'>{{ item }}</li>
+			</ul>
+		</v-alert>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'AppAlert',
+	props: {},
+	data: function() {
+		return {};
+	},
+	computed: {
+		alertObj() {
+			return this.$store.getters.getAlertObj;
+		}
+	}
+}
+</script>
+
+<style lang='scss' scoped>
+	.che-alert-error {
+		z-index: 1;
+	}
+</style>
