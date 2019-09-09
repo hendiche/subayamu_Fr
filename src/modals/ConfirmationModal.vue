08b31038a1308e87b781b8a7cfc304e46e2d2ef5
Unknown <talles_of_destiny@yahoo.com.sg>
2019-09-10 02:56:22 +0900

    Added & Created confirmation modal

---
+++ src/modals/ConfirmationModal.vue
@@ -0,0 +1,64 @@
+<template>
+	<v-dialog v-model='dialog' max-width='700px'>
+		<v-card>
+			<v-card-title>{{ dialogTitle }}</v-card-title>
+			<v-card-text>
+				<div style='white-space: pre-line'>{{ dialogBody }}</div>
+			</v-card-text>
+			<v-card-actions>
+				<v-spacer />
+				<v-btn
+					:color='dialogPosColor' 
+					@click='$emit("positifConfirm")'
+				>
+					{{ dialogPosBtn }}
+				</v-btn>
+
+				<v-btn @click='$emit("close")'>{{ dialogNegBtn }}</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+export default {
+	name: 'confirmationModal',
+	props: {
+		modal: Boolean,
+		title: String,
+		body: String,
+		positifBtn: String,
+		positifBtnColor: String,
+		negatifBtn: String,
+	},
+	data: function() {
+		return {};
+	},
+	computed: {
+		dialog: {
+			get() {
+				return this.modal;
+			},
+			set(val) {
+				if (!val) this.$emit('close');
+			}
+		},
+		dialogTitle() {
+			return this.title || '';
+		},
+		dialogBody() {
+			return this.body || '';
+		},
+		dialogPosBtn() {
+			return this.positifBtn || '';
+		},
+		dialogPosColor() {
+			return this.positifBtnColor || '';
+		},
+		dialogNegBtn() {
+			return this.negatifBtn || '';
+		},
+	},
+	methods: {},
+}
+</script>
